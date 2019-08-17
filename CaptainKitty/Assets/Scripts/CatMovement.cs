926e35c211bf4580d04da73726c6a460f2f37416
leea <leea.harlan@gmail.com>
2019-08-17 16:44:18 -0700

    Add cat sprinting

--- CaptainKitty/Assets/Scripts/CatMovement.cs
+++ CaptainKitty/Assets/Scripts/CatMovement.cs
@@ -1,23 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CatMovement : MonoBehaviour
 {
     Animator catAnimator; 
     [SerializeField] public float movementSpeed = 5.0f;
+    [SerializeField] public float sprintSpeed = 10.0f;
     [SerializeField] public float rotationSpeed = 200.0f;
     void Update () {
-
-        //float inputSpeed = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+        var speed = movementSpeed;
+        if (Input.GetKey("left shift") || Input.GetKey("right shift"))
+        {
+          speed = sprintSpeed;
+        }
 
         transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
-        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
 
         catAnimator = GetComponent<Animator>();
-        Debug.Log("Moving at speed: " + (Mathf.Abs(Input.GetAxis("Horizontal") * movementSpeed) + Mathf.Abs(Input.GetAxis("Vertical") * movementSpeed)));
+        Debug.Log("Moving at speed: " + (Mathf.Abs(Input.GetAxis("Horizontal") * speed) + Mathf.Abs(Input.GetAxis("Vertical") * speed)));
         catAnimator.SetFloat("MoveX", Input.GetAxis("Horizontal"));
         catAnimator.SetFloat("MoveY", Input.GetAxis("Vertical"));
-        catAnimator.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Horizontal")*movementSpeed) + (Mathf.Abs(Input.GetAxis("Vertical")*movementSpeed))));
+        catAnimator.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Horizontal")* speed) + (Mathf.Abs(Input.GetAxis("Vertical")* speed))));
     }
 }
