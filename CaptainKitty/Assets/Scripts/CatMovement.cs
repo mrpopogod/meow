fbb2843a3ea56fb47e4877cc1daff7d10a5d8f52
Trevor Housten <thousten@blizzard.com>
2019-08-17 16:37:07 -0700

    Added some buggy jumping.

--- CaptainKitty/Assets/Scripts/CatMovement.cs
+++ CaptainKitty/Assets/Scripts/CatMovement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CatMovement : MonoBehaviour
 {
-    Animator catAnimator; 
-    [SerializeField] public float movementSpeed = 5.0f;
+    Animator catAnimator;
+    private int jumpTimer = 0;
+
+    [SerializeField] public float movementSpeed = 2.0f;
     [SerializeField] public float rotationSpeed = 200.0f;
+    [SerializeField] public float jumpSpeed = 3.0f;
     void Update () {
 
         //float inputSpeed = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
@@ -19,5 +22,17 @@ public class CatMovement : MonoBehaviour
         catAnimator.SetFloat("MoveX", Input.GetAxis("Horizontal"));
         catAnimator.SetFloat("MoveY", Input.GetAxis("Vertical"));
         catAnimator.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Horizontal")*movementSpeed) + (Mathf.Abs(Input.GetAxis("Vertical")*movementSpeed))));
+
+        //if Fire 1 (jump) is pressed
+        if (Input.GetAxis("Jump") > 0.0f)
+        {
+            catAnimator.SetBool("Jump", true);
+
+            //transform.Rotate(0, jumpSpeed * Time.deltaTime * rotationSpeed, 0);
+            transform.Translate(0, jumpSpeed * Time.deltaTime, 0);//Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+        } if (Input.GetAxis("Jump") == 0.0f)
+        {
+            catAnimator.SetBool("Jump", false);
+        }
     }
 }
