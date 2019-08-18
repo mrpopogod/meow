4083ef6f2e5f01f6d957f5f5797b7034c3e4fb54
Aaron Doukas <amyleeslave@gmail.com>
2019-08-18 14:03:56 -0700

    In progress tree burning functionality

---
+++ CaptainKitty/Assets/TreeBurn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBurn : MonoBehaviour
+{
+    [SerializeField] private GameObject _stump;
+
+    public void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Burn();
+        }
+    }
+
+    public void Burn()
+    {
+        // TODO: a particle
+        var stump = Instantiate(_stump, gameObject.transform);
+        stump.transform.localScale = new Vector3(50f, 50f, 50f);
+        Destroy(gameObject);
+    }
+}
