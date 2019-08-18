5077f16afe851b066d4729ecba1a9beff92674c3
Trevor Housten <thousten@blizzard.com>
2019-08-18 16:30:17 -0700

    Kitty glows & Puzzle Pieces.

--- CaptainKitty/Assets/Scripts/PuzzlePiece.cs
+++ CaptainKitty/Assets/Scripts/PuzzlePiece.cs
@@ -7,5 +7,11 @@ public class PuzzlePiece : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name + " has entered the " + this.name);
+        //if (other)
+        if (other.name == "PuzzlePiece")
+        {
+            other.transform.position = this.transform.position;
+            other.attachedRigidbody.mass = 1000;
+        }
     }
 }
