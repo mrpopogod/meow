c10a9292d3835ec0727b52481a43a52ea21e67ba
Aaron Doukas <amyleeslave@gmail.com>
2019-08-17 17:02:00 -0700

    Merging jump fix with rest.

--- CaptainKitty/Assets/Scripts/CatMovement.cs
+++ CaptainKitty/Assets/Scripts/CatMovement.cs
@@ -2,13 +2,24 @@
 
 public class CatMovement : MonoBehaviour
 {
+
     Animator catAnimator; 
+    private int jumpTimer = 0;
+    private Rigidbody _rb;
+    private bool _isJumping;
+    private float _startHeight;
+
+    public void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _isJumping = false;
+        _startHeight = transform.position.y;
+    }
+
     [SerializeField] public float movementSpeed = 5.0f;
     [SerializeField] public float sprintSpeed = 10.0f;
     [SerializeField] public float rotationSpeed = 200.0f;
     [SerializeField] public float jumpSpeed = 3.0f;
-
-    private int jumpTimer = 0;
 
     void Update () {
         var speed = movementSpeed;
@@ -27,15 +38,22 @@ public class CatMovement : MonoBehaviour
         catAnimator.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Horizontal")* speed) + (Mathf.Abs(Input.GetAxis("Vertical")* speed))));
 
         //if Fire 1 (jump) is pressed
-        if (Input.GetAxis("Jump") > 0.0f)
+        if (Input.GetAxis("Jump") > 0.0f && !_isJumping)
         {
             catAnimator.SetBool("Jump", true);
-            transform.Translate(0, jumpSpeed * Time.deltaTime, 0);
+            _isJumping = true;
+            _rb.velocity = new Vector3(_rb.velocity.x, jumpSpeed, _rb.velocity.z);
+            //transform.Rotate(0, jumpSpeed * Time.deltaTime * rotationSpeed, 0);
+            //transform.Translate(0, jumpSpeed * Time.deltaTime, 0);//Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
         }
 
-        if (Input.GetAxis("Jump") == 0.0f)
+        if (transform.position.y <= _startHeight)
         {
-            catAnimator.SetBool("Jump", false);
+            _isJumping = false;
         }
+        //if (Input.GetAxis("Jump") == 0.0f)
+        //{
+        //    catAnimator.SetBool("Jump", false);
+        //}
     }
 }
