4d3cc40f90a539114e0f361d1ac535aaba932044
leea <leea.harlan@gmail.com>
2019-08-18 12:58:13 -0700

    Initial game state scripts

--- CaptainKitty/Assets/Scripts/Cat.cs
+++ CaptainKitty/Assets/Scripts/Cat.cs
@@ -3,16 +3,19 @@
 public class Cat : MonoBehaviour
 {
   [SerializeField] CatMovement movement;
+  private GameProgress progress = new GameProgress();
 
   // Use this for initialization
   void Start()
   {
     movement = gameObject.AddComponent<CatMovement>();
+    progress.ChangeState(new InitialState());
   }
 
   // Update is called once per frame
   void Update()
   {
     movement.Update();
+    progress.RunCurrentState();
   }
 }
---
+++ CaptainKitty/Assets/Scripts/GameState/LevelState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Level
+{
+  Water,
+  Fire,
+  Earth,
+  Wind
+}
+
+public class LevelState : IState
+{
+  private Level currentLevel;
+
+  public LevelState(Level newLevel)
+  {
+    currentLevel = newLevel;
+  }
+  
+  public void Enter()
+  {
+    Debug.Log($"Entering Level State at Level {currentLevel}");
+  }
+
+  public void Execute()
+  {
+    
+  }
+
+  public void Exit()
+  {
+    Debug.Log("Entering Initial State");
+  }
+}
