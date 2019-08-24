1e4e57e44996f0f685b176b69b3d55dac85045cb
Trevor Housten <thousten@blizzard.com>
2019-08-24 14:41:11 -0700

    Fixed dialog scaling.

--- CaptainKitty/Assets/Scripts/BlowableInteractable.cs
+++ CaptainKitty/Assets/Scripts/BlowableInteractable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BlowableInteractable : MonoBehaviour
 {
-    public int myForce = 100;
+    public int myForce = 10;
     public void BlowWithForce(Vector3 force)
     {
         //float horizontal = Input.GetAxis("Horizontal"); //how much the cat should turn local left or right
@@ -12,12 +12,16 @@ public class BlowableInteractable : MonoBehaviour
         //Vector3 rotationVector = new Vector3(0, horizontal * rotationSpeed, 0);
         //Quaternion deltaRotation = Quaternion.Euler(rotationVector * Time.deltaTime);//new Quaternion(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0f, 1f);
         Rigidbody _rb = this.GetComponent<Rigidbody>();
-        if (_rb == null) return;
+        if (_rb == null)
+        {
+            Debug.Log("There's an issue with the Rigidbody on this object...");
+            return;
+        }
 
         //Vector3 position = _rb.position;
 
         //float moveDistance = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        _rb.AddForceAtPosition(force, _rb.position);
+        _rb.AddForceAtPosition(force*myForce, _rb.position);
         //_rb.MovePosition((_rb.position) + (_transform.forward * moveDistance));// (1-Input.GetAxis("Horizontal")));//(Input.GetAxis("Horizontal") * speed * Time.deltaTime));
         //_rb.MoveRotation(_rb.rotation * deltaRotation);
     }
