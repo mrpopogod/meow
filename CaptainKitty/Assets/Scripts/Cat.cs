43945c701710c8e89c042a94d6a73cabb6281aad
Trevor Housten <thousten@blizzard.com>
2019-08-24 17:28:22 -0700

    Cat's skin changes color to show progression through the zones (blue feet is super hard to notice)

--- CaptainKitty/Assets/Scripts/Cat.cs
+++ CaptainKitty/Assets/Scripts/Cat.cs
@@ -4,25 +4,58 @@ public class Cat : MonoBehaviour
 {
     private CatMovement movement;
     public GameObject firePrefab;
-	public GameObject windPrefab;
+    public GameObject windPrefab;
     private bool inDesert = false;
     private bool inForest = false;
     private bool inRiver = false;
     private bool inGorge = false;
+    public bool canFire = false;
+    public bool canWind = false;
+    public bool canWater = false;
+    public GameObject catObject = null;
+    public SkinnedMeshRenderer myMesh = null;
+    public Material BaseSkin = null;
+    public Material WaterSkin = null;
+    public Material FireSkin = null;
+    public Material WindSkin = null;
 
     private int progress = 0;
-
+    public void ChangeSkin (int newNumber)
+    {
+        Material[] mats = myMesh.materials;
+        if (newNumber == 0)
+        {
+            mats[0] = BaseSkin;
+        } else if (newNumber == 1)
+        {
+            mats[0] = WaterSkin;
+        }
+        else if (newNumber == 2)
+        {
+            mats[0] = FireSkin;
+        }
+        else if (newNumber == 3)
+        {
+            mats[0] = WindSkin;
+        }
+        myMesh.materials = mats;
+    }
     // Use this for initialization
     void Start()
     {
         movement = new CatMovement(GetComponent<Rigidbody>(), GetComponent<Animator>(), GetComponent<Transform>(), firePrefab, windPrefab);
         GameProgress.Progress.ChangeState(new InitialState());
+
+        //TODO: If the catObject is null, assign it to the proper child.
+        myMesh = catObject.GetComponent<SkinnedMeshRenderer>();//this.transform.FindChild("skeleton").transform.GetComponent(SkinnedMeshRenderer);
+        progress = 0;
     }
 
     void Update()
     {
         movement.Update();
         GameProgress.Progress.RunCurrentState();
+        //Renderer.
     }
 
     public void EnteredDesert()
@@ -30,7 +63,16 @@ public class Cat : MonoBehaviour
         inDesert = true;
         if (progress == 1)
         {
+            Debug.Log("Cat entered the Desert correctly.");
             progress = 2;
+            canFire = true;
+            //Shader myShader = catObject.GetComponent<Shader>();
+            //Texture myTexture = null;
+            //catObject.renderer.material.SetTexture();
+
+            ChangeSkin(2);
+            //mats[0] = headMat;
+            //skeleton.materials = mats;
         }
     }
 
@@ -44,7 +86,10 @@ public class Cat : MonoBehaviour
         inForest = true;
         if (progress == 2)
         {
+            Debug.Log("Cat entered the Forest correctly.");
             progress = 3;
+            canWind = true;
+            ChangeSkin(3);
         }
     }
 
@@ -73,7 +118,11 @@ public class Cat : MonoBehaviour
         inRiver = true;
         if (progress == 0)
         {
+            Debug.Log("Cat entered the river correctly.");
             progress = 1;
+            canWater = true;
+
+            ChangeSkin(1);
         }
     }
 
